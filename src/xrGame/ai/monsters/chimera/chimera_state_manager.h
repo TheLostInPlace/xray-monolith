f9192b41191dce4501e7a09f5b87dd584bf5c785
TheLostInPlace <42595640+TheLostInPlace@users.noreply.github.com>
2026-09-16 17:01:48 -0500

    ai: chimera threaten and hitted states behind config keys

--- src/xrGame/ai/monsters/chimera/chimera_state_manager.h
+++ src/xrGame/ai/monsters/chimera/chimera_state_manager.h
@@ -8,9 +8,15 @@ class CStateManagerChimera : public CMonsterStateManager<CChimera>
 private:
 	typedef CMonsterStateManager<CChimera> inherited;
 
+	bool m_threaten_enabled;
+	bool m_hitted_enabled;
+
 public:
 	CStateManagerChimera(CChimera* obj);
 	virtual ~CStateManagerChimera();
+
+	// registers the optional threaten and hitted states when their keys are on
+	void load_optional_states(LPCSTR section);
 
 	virtual void execute();
 	virtual void remove_links(CObject* object) { inherited::remove_links(object); }
